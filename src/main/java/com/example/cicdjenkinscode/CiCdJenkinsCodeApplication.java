4921778fa789516dae47154d430b4c0586df0d5e
yhnoh <tedgom19@gmail.com>
2022-10-16 19:17:32 +0900

    learn ci-cd code from jenkins

---
+++ src/main/java/com/example/cicdjenkinscode/CiCdJenkinsCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdjenkinscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdJenkinsCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdJenkinsCodeApplication.class, args);
+    }
+
+}
